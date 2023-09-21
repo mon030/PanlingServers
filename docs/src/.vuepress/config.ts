31609635190d1f99bd35cdfd0aebba79e93fc84a
MON <42599449+mon030@users.noreply.github.com>
2023-09-20 21:43:18 -0500

    update & add search plugin

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -4,9 +4,8 @@ import { defaultTheme } from "@vuepress/theme-default";
 import { commentPlugin } from "vuepress-plugin-comment2";
 import { defineUserConfig } from "vuepress";
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
-
-
-
+import { defineUserConfig } from "vuepress";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 
 export default defineUserConfig({
@@ -31,7 +30,21 @@ export default defineUserConfig({
        googleAnalyticsPlugin({
       id: 'G-EFWFD2M8G0',
     }),
-    
+searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
   ],
   
     sidebar: "heading",
